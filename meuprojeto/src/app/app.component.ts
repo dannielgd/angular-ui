5e6ad834b640086432ad88879c8a4eda1a520e58
dannielgd <dannielgd@gmail.com>
2019-11-03 15:15:14 -0300

    Criando Funcionários e colocando fotos dinamicamente

--- meuprojeto/src/app/app.component.ts
+++ meuprojeto/src/app/app.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
+import {
+	Component
+} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nome = 'Danniel';
-  adicionado = false;
+	ultimoId = 0;
+	nome = 'Danniel';
+	adicionado = false;
+	funcionarios = [];
 
-  adicionar() {
-	console.log(`Adicionando ${this.nome}`);
-	this.adicionado = true;
+	adicionar() {
+		console.log(`Adicionando ${this.nome}`);
+		this.adicionado = true;
 
-  }
+		this.funcionarios.push({
+			id: ++this.ultimoId,
+			nome: this.nome
+		});
+	}
 }
