5b6a0a0fc16595a0773945b2e02f4208f15f240c
Danniel <dannielgd@gmail.com>
2019-11-04 10:16:23 -0300

    C

---
+++ meuprojeto/src/app/funcionario-form/funcionario-form.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-funcionario-form',
+  templateUrl: './funcionario-form.component.html',
+  styleUrls: ['./funcionario-form.component.css']
+})
+export class FuncionarioFormComponent{
+
+	ultimoId = 0;
+	nome = 'Danniel';
+	adicionado = false;
+	@Output('criado') funcionarioAdicionado = new EventEmitter();
+	
+	adicionar() {
+		console.log(`Adicionando ${this.nome}`);
+		this.adicionado = true;
+
+		const funcionario = {
+			id: ++this.ultimoId,
+			nome: this.nome
+		};
+
+		this.funcionarioAdicionado.emit(funcionario); //funcionario vai para a variavel $event lá no html
+	}
+}
