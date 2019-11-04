3ab7741f9ab0cfd80d77d8f45c7c36d2b4229f50
Danniel <dannielgd@gmail.com>
2019-11-04 10:02:58 -0300

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 15d3cad832115923fba13606c3410d73b00ffa3e.

--- meuprojeto/src/app/app.component.ts
+++ meuprojeto/src/app/app.component.ts
@@ -1,18 +1,15 @@
-import {
-	Component
-} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  nome = 'Danniel';
 
-	funcionarios = [];
-
-	aoAdicionar(funcionario) {
-		this.funcionarios.push(funcionario);
-	}
-
+  adicionar(nome: any) {
+    this.nome = nome;
+    
+  }
 }
