fe3cdcbff81abcd43a25543fa00e60494989f3f8
dannielgd <dannielgd@gmail.com>
2019-11-07 20:01:45 -0300

    Update

--- meuprojeto/src/app/funcionario-card/funcionario-card.component.ts
+++ meuprojeto/src/app/funcionario-card/funcionario-card.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
 	selector: 'app-funcionario-card',
 	templateUrl: './funcionario-card.component.html',
-	//styleUrls: ['./funcionario-card.component.css']
+	// styleUrls: ['./funcionario-card.component.css']
 	styles: [`
 	.card-body {
 		text-transform: uppercase;
