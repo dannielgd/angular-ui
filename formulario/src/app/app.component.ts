1992b7c1c8e71e2637c5d489c8c365083b7cca80
Danniel <dannielgd@gmail.com>
2019-11-15 15:20:43 -0300

    Oneway binding - Aula 12.5

--- formulario/src/app/app.component.ts
+++ formulario/src/app/app.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import {
+    Component
+} from '@angular/core';
+import {
+    NgForm
+} from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
-  profissoes= ['Programador', 'Empresário', 'Outra'];
+	profissoes = ['Programador', 'Empresário', 'Outra'];
+	profissao = 'Outra';
 
-  salvar(form: NgForm) {
-    console.log(form);
+    salvar(form: NgForm) {
+		console.log(this.profissao);
 
-  }
+		console.log(form.value.profissao);
+
+    }
 }
