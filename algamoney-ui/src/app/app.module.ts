f553d408ae1a368828773f39894791e377a3c360
Danniel <dannielgd@gmail.com>
2019-11-19 18:13:59 -0300

    Aulas 13.1 e início da 13.2

--- algamoney-ui/src/app/app.module.ts
+++ algamoney-ui/src/app/app.module.ts
@@ -1,17 +1,24 @@
-import { browser } from 'protractor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { InputTextModule } from 'primeng/components/inputtext/inputtext';
 import { ButtonModule } from 'primeng/components/button/button';
+import { CalendarModule } from 'primeng/components/calendar/calendar';
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+import { InputTextareaModule } from 'primeng/components/inputtextarea/inputtextarea';
 import { TableModule } from 'primeng/components/table/table';
 import { TooltipModule } from 'primeng/components/tooltip/tooltip';
 
 import { AppComponent } from './app.component';
+
+import { CampoColoridoDirective } from './campo-colorido.directive';
+
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
-import { CampoColoridoDirective } from './campo-colorido.directive';
+
+
 
 @NgModule({
 	declarations: [
@@ -19,14 +26,18 @@ import { CampoColoridoDirective } from './campo-colorido.directive';
 		LancamentosPesquisaComponent,
 		NavbarComponent,
 		PessoasPesquisaComponent,
-		CampoColoridoDirective
+		CampoColoridoDirective,
+		LancamentoCadastroComponent
 	],
 	imports: [
 		BrowserModule,
+		BrowserAnimationsModule,
 		InputTextModule,
 		ButtonModule,
 		TableModule,
 		TooltipModule,
+		InputTextareaModule,
+		CalendarModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
