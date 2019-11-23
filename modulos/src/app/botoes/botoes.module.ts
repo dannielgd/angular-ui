5c73f2b8362589c3ec7a247f368f171be607a6b4
Danniel <dannielgd@gmail.com>
2019-11-23 16:27:21 -0300

    Módulos - Aula 14.1

---
+++ modulos/src/app/botoes/botoes.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BotaoGrandeComponent } from './botao-grande/botao-grande.component';
+
+
+
+@NgModule({
+  declarations: [BotaoGrandeComponent],
+  exports: [BotaoGrandeComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class BotoesModule { }
