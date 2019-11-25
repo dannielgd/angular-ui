88e85f90fd585b8d6c34763da84c9191395b74d7
Danniel <dannielgd@gmail.com>
2019-11-25 18:03:02 -0300

    Desafio Feature Module Pessoa

--- algamoney-ui/src/app/app.module.ts
+++ algamoney-ui/src/app/app.module.ts
@@ -3,54 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { ButtonModule } from 'primeng/components/button/button';
-import { CalendarModule } from 'primeng/components/calendar/calendar';
-import {SelectButtonModule} from 'primeng/components/selectbutton/selectbutton';
-import { InputTextModule } from 'primeng/components/inputtext/inputtext';
-import { InputTextareaModule } from 'primeng/components/inputtextarea/inputtextarea';
-import { TableModule } from 'primeng/components/table/table';
-import { TooltipModule } from 'primeng/components/tooltip/tooltip';
-import { DropdownModule } from 'primeng/components/dropdown/dropdown';
-import {CurrencyMaskModule} from 'ng2-currency-mask';
-import {InputMaskModule} from 'primeng/components/inputmask/inputmask';
-
 import { AppComponent } from './app.component';
 
 import { CampoColoridoDirective } from './campo-colorido.directive';
 
 import { NavbarComponent } from './navbar/navbar.component';
-import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
-import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
 import { MessageComponent } from './message/message.component';
-import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
+import { PessoasModule } from './pessoas/pessoas.module';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		NavbarComponent,
-		PessoasPesquisaComponent,
 		CampoColoridoDirective,
-		PessoaCadastroComponent,
 		MessageComponent,
-		PessoasGridComponent
 	],
 	imports: [
 		BrowserModule,
 		BrowserAnimationsModule,
-		FormsModule,
 
 		LancamentosModule,
-
-		InputTextModule,
-		ButtonModule,
-		TableModule,
-		TooltipModule,
-		InputTextareaModule,
-		CalendarModule,
-		SelectButtonModule,
-		DropdownModule,
-		InputMaskModule
+		PessoasModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
---
+++ algamoney-ui/src/app/pessoas/pessoas.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
+import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component';
+
+import { InputMaskModule } from 'primeng/components/inputmask/inputmask';
+import { DropdownModule } from 'primeng/components/dropdown/dropdown';
+import { SelectButtonModule } from 'primeng/components/selectbutton/selectbutton';
+import { CalendarModule } from 'primeng/components/calendar/calendar';
+import { TooltipModule } from 'primeng/components/tooltip/tooltip';
+import { TableModule } from 'primeng/components/table/table';
+import { ButtonModule } from 'primeng/components/button/button';
+import { InputTextModule } from 'primeng/components/inputtext/inputtext';
+
+@NgModule({
+  declarations: [
+	  PessoaCadastroComponent,
+	  PessoasPesquisaComponent,
+	  PessoasGridComponent
+  ],
+  imports: [
+	CommonModule,
+	FormsModule,
+
+	InputTextModule,
+	ButtonModule,
+	TableModule,
+	TooltipModule,
+	CalendarModule,
+	SelectButtonModule,
+	DropdownModule,
+	InputMaskModule
+  ],
+  exports: [
+	PessoaCadastroComponent,
+	PessoasPesquisaComponent
+  ]
+})
+export class PessoasModule { }
