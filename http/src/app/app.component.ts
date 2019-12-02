ee624de613631f7270015945332aa0ff5a236f15
Danniel <dannielgd@gmail.com>
2019-12-02 05:57:59 -0300

    Até Aula 16.2

--- http/src/app/app.component.ts
+++ http/src/app/app.component.ts
@@ -7,12 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  cidades = [
-    { id: 1, nome: 'Uberlândia' },
-    { id: 2, nome: 'São Paulo' },
-    { id: 3, nome: 'Florianópolis' },
-    { id: 4, nome: 'Curitiba' }
-  ];
+  cidades = [ ];
 
   adicionar(nome: string) {
     alert(nome);
