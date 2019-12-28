61e1e1bdf73cd683f3fff86e0ee23278a32093a7
Danniel <dannielgd@gmail.com>
2019-12-28 14:21:25 -0300

    Excluindo um registro sem mostrar mensagem-17.8

--- algamoney-ui/src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ algamoney-ui/src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -1,7 +1,8 @@
 import { LancamentoFiltro } from './../lancamento.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { LancamentoService } from '../lancamento.service';
 import {LazyLoadEvent} from 'primeng/components/common/api';
+import { Table } from 'primeng/components/table/table';
 
 @Component({
   selector: 'app-lancamentos-pesquisa',
@@ -13,6 +14,7 @@ export class LancamentosPesquisaComponent implements OnInit {
 	totalRegistros = 0;
 	filtro = new LancamentoFiltro();
 	lancamentos = [ ];
+	@ViewChild('tabela', {static: true}) grid: Table;
 
 	constructor(private lancamentoService: LancamentoService) { }
 
@@ -32,5 +34,12 @@ export class LancamentosPesquisaComponent implements OnInit {
 	aoMudarPagina(event: LazyLoadEvent) {
 		const pagina = event.first / event.rows;
 		this.pesquisar(pagina);
+	}
+
+	excluir(lancamento: any) {
+		this.lancamentoService.excluir(lancamento.codigo)
+		  .then(() => {
+			this.grid.reset(); //exibe a primeria pagina, resetando a tabela
+		  });
 	}
 }
